7f03f8853f4bb788048ebd50cb798691775381be
WhiteDragonX299 <whitedragonx299@gmail.com>
2022-07-16 21:56:35 +0800

    Buug fixes

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,15 +1,20 @@
 let push = document.getElementById("push");
 const addtask = () => {
-	console.log(`button have been clicked`);
+	console.log(`button have been loaded`);
 	let task = document.getElementById("inputvalue").value;
-	const li = document.createElement("li").innerHTML = `${task}`;
-	li.appendChild;
-	console.log(`${task} have been added`);
-	task.value = "";
-	if (task === "") {
-		alert(`Just write something`);
-	} else {
-		return;
-	}
+	const li = document.createElement('li');
+	li.innerHTML = `${task}`;
+	document.getElementById("ol").appendChild(li); 
+	console.log(`${task} have been loaded`);
+	// const h3 = document.getElementById("h3");
+	// let x = document.getElementById("li");
+	// let number = x
+	// h3.innerHTML = `You now have ${number} tasks`
 };
+
+// if (task === "") {
+// 	alert(`Just write something`);
+// } else {
+// 	return;
+// }
 push.addEventListener("click", addtask());
