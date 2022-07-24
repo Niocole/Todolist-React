798974cf5cb8c3790f26667917e7edeb29f2e0bf
WhiteDragonX299 <whitedragonx299@gmail.com>
2022-07-24 13:22:11 +0800

    jQuery work

---
+++ public/js/todo.js
@@ -0,0 +1,23 @@
+//
+let count = $("#count").length;
+let gtask = "task";
+if(count = 1) {
+    gtask = "task"
+} else {
+    gtask="tasks"
+}
+//callback function
+$(document).ready(function(){
+    addtask();
+    upno();
+})
+//Add task
+const addtask = () => {
+    $("addtask").click(function(){
+        
+    })
+}
+//Update number of task
+const upno = () => {
+    $("")
+}
